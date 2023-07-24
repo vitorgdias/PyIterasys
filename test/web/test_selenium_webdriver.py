59b6bbe2d72f0c5d7c8668b35be85ebce3326c97
vitorgdias <vitorgalves@live.com>
2023-07-24 13:21:06 -0300

    Used parametrize to set up more than one search.

--- test/web/test_selenium_webdriver.py
+++ test/web/test_selenium_webdriver.py
@@ -12,15 +12,19 @@ class Test_Selenium_Webdriver:
         self.driver.maximize_window()
     def teardown_method(self):
         self.driver.quit()
-
-    def test_python_course(self):
+    @pytest.mark.parametrize('term, course, price',[
+        ('python', 'Introdução ao Python', 'R$ 27,90'),
+        ('Introdução ao Java', 'Introdução ao Java', 'R$ 27,90'),
+    ])
+    def test_python_course(self, term, course, price):
         self.driver.get('https://www.iterasys.com.br')
         self.driver.find_element(By.ID, '16237702146520').click()
         self.driver.find_element(By.XPATH, '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/input[1]').click()
-        self.driver.find_element(By.XPATH, '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/input[1]').send_keys('Python')
+        self.driver.find_element(By.XPATH, '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/input[1]').clear()
+        self.driver.find_element(By.XPATH, '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/input[1]').send_keys(term)
         #self.driver.find_element(By.XPATH, '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/main[1]/a[1]/main[1]/h3[1]').click()
         wait = WebDriverWait(self.driver, 10)
-        element = wait.until(EC.text_to_be_present_in_element((By.XPATH, '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/main[1]/a[1]/main[1]/h3[1]'), 'Introdução ao Python'))
+        element = wait.until(EC.text_to_be_present_in_element((By.XPATH, '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/main[1]/a[1]/main[1]/h3[1]'), course))
         self.driver.find_element(By.XPATH,'/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/main[1]/a[1]/main[1]/h3[1]').click()
-        assert self.driver.find_element(By.XPATH, '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/p[1]').text == 'R$ 27,90'
-        assert self.driver.find_element(By.XPATH, '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/h1[1]').text == 'Introdução ao Python'
+        assert self.driver.find_element(By.XPATH, '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/p[1]').text == price
+        assert self.driver.find_element(By.XPATH, '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/h1[1]').text == course
